364a6c9121e216a2694bb01dcc07a00de10432c6
Pablo Robles <pablorom_11@hotmail.com>
2026-03-05 13:33:36 +0100

    feat: add initial files

---
+++ script.js
@@ -0,0 +1,48 @@
+// ============================
+// SALUDO PERSONALIZADO
+// ============================
+
+function mostrarMensaje() {
+  let nombre = document.getElementById("nombre").value;
+
+  // TODO: mostrar mensaje en pantalla
+}
+
+// ============================
+// CALCULAR MEDIA
+// ============================
+
+function calcularMedia() {
+  let n1 = document.getElementById("nota1").value;
+  let n2 = document.getElementById("nota2").value;
+  let n3 = document.getElementById("nota3").value;
+
+  // TODO:
+  // convertir a número
+  // calcular media
+  // mostrar resultado
+}
+
+// ============================
+// MODO OSCURO
+// ============================
+
+function modoOscuro() {
+  // TODO:
+  // cambiar color fondo
+}
+
+// ============================
+// FRASE MOTIVADORA
+// ============================
+
+function fraseMotivadora() {
+  let frases = [
+    "Sigue aprendiendo",
+    "El esfuerzo merece la pena",
+    "Cada error enseña algo",
+  ];
+
+  // TODO:
+  // elegir frase aleatoria
+}
